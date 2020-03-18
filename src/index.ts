e8e74a74c08d3d6aa2586dce8a4f6a6f3de5217c
Alexander Zeitler <azeitler@vote2work.com>
2020-03-18 20:29:49 +0100

    feat: pass conditions from commandline

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,6 @@
-const conditions = 3
+import { createDecisionTable } from './createDecisionTable'
 
-// write md table headers
+const conditions = Number(process.argv[2])
 console.log(`${Array(conditions + 3).join('| ')}|`)
 console.log(`${Array(conditions + 3).join('|-')}|`)
-
-let row = 1
-for (let i = Math.pow(2, conditions) - 1; i >= 0; i--) {
-  let binaryNumber = i.toString(2)
-
-  // fill with leading zeros
-  binaryNumber = binaryNumber.padStart(conditions, '0')
-
-  let flags: string[] = []
-
-  const bits = binaryNumber.split('')
-
-  flags = [
-    row.toString(),
-    ...flags,
-    ...bits.map(bit => (bit === '1' ? 'x' : '-'))
-  ]
-  console.log(`|${flags.join('|')}||`)
-  row++
-}
+console.log(createDecisionTable(conditions).join('\n'))
